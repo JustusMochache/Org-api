b56f02cb24ab7ec33b5d00c85f00133790ef6552
MoringaSchool <you@example.com>
2020-01-20 14:10:24 +0300

    add port connect in app.java class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
 public class App {
+
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        }else {
+            port = 4567;
+        }
+        port(port);
 }
